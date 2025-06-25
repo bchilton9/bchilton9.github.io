9da31a9a6f743cc2bd4e68ca4c510066603a5ede
bchilton9 <48000771+bchilton9@users.noreply.github.com>
2025-06-25 12:49:11 -0600

    Delete TheGadgetGuide/includes/footer.php

--- TheGadgetGuide/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-black text-center text-secondary py-4 mt-5">
-    <p>&copy; <?php echo date("Y"); ?> The Gadget Guide. All rights reserved.</p>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
