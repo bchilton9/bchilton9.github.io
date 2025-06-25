5159b95a01f92de821d286c4bb70dc326f18b198
bchilton9 <48000771+bchilton9@users.noreply.github.com>
2025-06-25 12:47:46 -0600

    Delete TheGadgetGuide/about.php

--- TheGadgetGuide/about.php
+++
@@ -1,6 +0,0 @@
-<?php include("includes/header.php"); ?>
-<div class="container mt-5">
-    <h1>About Us</h1>
-    <p>The Gadget Guide is dedicated to providing honest and helpful content for tech enthusiasts of all levels.</p>
-</div>
-<?php include("includes/footer.php"); ?>
