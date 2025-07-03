4f0b1de54266baab3669d6648ef11f377dcfc2c0
bchilton9 <48000771+bchilton9@users.noreply.github.com>
2025-07-03 12:11:02 -0600

    index.html

--- script.js
+++ script.js
@@ -140,3 +140,17 @@ typeLine();
     startMatrix();
   });
 });
+
+const backToTop = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
